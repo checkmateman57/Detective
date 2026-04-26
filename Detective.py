006f979ac6f6aee2f316c3971e2abf00e71af1b7
checkmateman57 <chris8457@outlook.com>
2026-04-26 12:26:23 -0600

    Bedroom inspection

--- Detective.py
+++ Detective.py
@@ -9,7 +9,7 @@ play = True
 action = 1
 while play == True:
     if action == 1:
-        game = input('Would you like to play?')
+        game = input('Would you like to play? ')
         if game.lower() == 'yes':
             action = 2
         elif game.lower() == 'no':
@@ -18,15 +18,18 @@ while play == True:
             print('Your answer must be yes or no.')
 
     if action == 2:
-        decision =  input('What would you like to do? A: Look at the victim  B: Investigate the crime scene  C: Interrogate the suspects ')
+        decision =  input('What would you like to do? A: Look at the victim  B: Investigate the crime scene  C: Interrogate the suspects \n')
         if decision.lower() == 'a':
             action = 3
+        elif decision.lower() == 'b':
+            action = 4
 
 
     if action == 3:
-        decision = input('What would you like to do? A: Look at the autopsy report  B: Look at his medical records  C: Read his story  D: Go back ')
+        decision = input('What would you like to do? A: Look at the autopsy report  B: Look at his medical records  C: Read his story  D: Go back \n')
         if decision.lower() == 'a':
             print('The autopsy report reveals that he died due to suffocation.\n'
+                  'He was found by the servant in his underwear.'
                   'When examining the body, there appeared to be grip marks and scratch marks around his throat.\n'
                   'His toxicology report indicates increased levels of histamine.\n')
             time.sleep(1)
@@ -36,8 +39,8 @@ while play == True:
                   'Lastly, he had a knee replacement surgery 2 years ago.\n')
             time.sleep(1)
         elif decision.lower() == 'c':
-            print("Alistair Vane was a man who was loved by many and hated by few. At the young age of 15, he figured out a way to harness the energy from the Earth's core"
-                  " and turn it into electricity, making energy sources like fossil fuels and solar power useless.\n"
+            print("Alistair Vane was a man who was loved by many and hated by few. At the young age of 15, he figured out a way to harness the energy from the Earth's core\n"
+                  "and turn it into electricity, making energy sources like fossil fuels and solar power useless.\n"
                   "Naturally, he got very rich, but money made him into a bad person. For those that knew him, he acted like he was far superior to everyone else,"
                   "and made sure he treated anyone other than his family as inferior.\n"
                   "After he got rich, Alistair started multiple other businesses for fun, and in recent news he cut off one of his partners for undisclosed reasons.")
@@ -46,3 +49,44 @@ while play == True:
             action = 2
         else:
             print('Your answer must be A, B, C, or D.')
+
+
+    if action == 4:
+        decision = input('Which room would you like to inspect? A: The bedroom  B: The kitchen  C: The bathroom  D: The living room  E: Go back \n')
+        if decision.lower() == 'a':
+            action = 5
+        elif decision.lower() == 'b':
+            action = 6
+        elif decision.lower() == 'c':
+            action = 7
+        elif decision.lower() == 'd':
+            action = 8
+        elif decision.lower() == 'e':
+            action = 2
+        else:
+            print('Your answer must be A, B, C, D, or E')
+
+    if action == 5:
+        decision = input('Which object would you like to inspect? A: The night stand  B: The closet  C: A journal  D: A suspicious looking wall  E: Go back \n')
+        if decision.lower() == 'a':
+            print("When looking through the night stand, you find an empty bottle of his blood pressure pills.")
+        elif decision.lower() == 'b':
+            print("When you open the closet, it is filled with pristine clothes. However, you notice that one of the shirts hanging seems to be stained with marinara sauce.\n")
+            time.sleep(1)
+        elif decision.lower() =='c':
+            print("Journals are always great to find clues. You go the most recent entries and find one dated the day he died.\n"
+                  "It reads: One of my old business partners came today to have a chat with me. We had a nice meal and drink, and then he went on his way.\n")
+            time.sleep(2)
+            print('You decide to go to another entry a few days ago that reads:\n'
+                  ' My daughter came today asking for money to buy a designer purse. I declined, she needs to learn how to make money. She was very upset with me.\n')
+            time.sleep(2)
+        elif decision.lower() == 'd':
+            print('You feel along a wall and notice some hairline gaps in the shape of a square. You knock in that area, and it feels a little hollow.\n'
+                  'After more searching, you find a button underneath the bed and press it, and the area along the wall opens.\n'
+                  'In it, you find blocks of cash along with a note: Emergency funds, $100 000 in this location.\n'
+                  'You count the money: Exactly $100 000.')
+            time.sleep(3)
+        elif decision.lower() == 'e':
+            action = 4
+        else:
+            print('Your answer must be A, B, C, D or E')
